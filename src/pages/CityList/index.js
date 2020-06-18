c5028102bc3e749796747ea9774e1eecdb5c50f0
qjj-0409 <qi18790396124@163.com>
2020-06-18 13:20:31 +0800

    城市列表-追加热门城市到城市列表

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -11,19 +11,38 @@ export default class CityList extends Component {
       const { data } = await axios.get('http://api-haoke-dev.itheima.net/area/city?level=1')
       // console.log(data.body)
       // 遍历城市列表，格式化数据
-      var cityList = {}
-      data.body.forEach((item) => {
+      const { cityList, cityWord} = this.formatCityList(data.body)
+      // 发请求获取热门城市
+      const res = await axios.get('http://api-haoke-dev.itheima.net/area/hot')
+      console.log('热门城市：',res.data.body)
+      // 追加热门城市到城市列表
+      cityList['hot'] = res.data.body
+      cityWord.unshift('hot')
+      console.log('城市列表：', cityList)
+      console.log('城市单词列表：', cityWord)
+    }
+
+    // 封装函数-格式化城市列表
+    formatCityList (list) {
+      // 遍历城市列表，格式化数据
+      let cityList = {}
+      list.forEach((item) => {
         // 截取每一项short属性值的第一个字符
-        var key = item.short.charAt(0)
+        let key = item.short.charAt(0)
         // 如果字符属性在对象中不存在，将当前这个城市封装为数组赋值给城市列表
         if (cityList[key] === undefined) {
-          cityList[key] = [item.label]
+          cityList[key] = [item]
         } else {
           // 如果字符属性在对象中存在，将当前这个城市追加到字符属性值中
-          cityList[key].push(item.label)
+          cityList[key].push(item)
         }
       })
-      console.log(cityList)
+      // 右侧单词列表
+      let cityWord = Object.keys(cityList).sort()
+      return {
+        cityList,
+        cityWord
+      }
     }
 
     // 生命周期-初次渲染到页面
