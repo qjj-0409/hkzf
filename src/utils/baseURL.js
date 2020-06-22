8a282885a5d59836be707f0511da118d5a4bea30
qjj-0409 <qi18790396124@163.com>
2020-06-22 18:59:22 +0800

    优化-配置生产环境和开发环境的接口地址

---
+++ src/utils/baseURL.js
@@ -0,0 +1,3 @@
+// 封装接口基准地址
+let baseURL = process.env.REACT_APP_URL
+export default baseURL
--- src/utils/request.js
+++ src/utils/request.js
@@ -4,10 +4,13 @@
 // 1.导入axios
 import axios from 'axios'
 
+// 导入接口基准地址
+import baseURL from './baseURL'
+
 // 2.创建axios实例对象
 const request = axios.create({
     // 配置基准地址
-    baseURL: 'http://api-haoke-web.itheima.net/'
+    baseURL
 })
 
 // 3.导出实例对象
